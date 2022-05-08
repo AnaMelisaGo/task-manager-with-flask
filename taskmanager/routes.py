695bd1b20117074fd3b84438e0e1504c5064ce8a
AnaMelisaGo <melisago.rockn@gmail.com>
2022-05-08 14:41:07 +0000

    Add new route for the database

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -9,3 +9,19 @@ def home():
     To render the base.html
     """
     return render_template("tasks.html")
+
+
+@app.route("/categories")
+def categories():
+    """
+    To render the categories.html
+    """
+    return render_template("categories.html")
+
+
+@app.route("/add_category", method=["GET", "POST"])
+def add_category():
+    """
+    To render the add category
+    """
+    return render_template("add_category.html")
