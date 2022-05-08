f0eba56f306db1117a646bc55bca2db23cff6b83
AnaMelisaGo <melisago.rockn@gmail.com>
2022-05-08 19:51:59 +0000

    Create cards to view all the categories from the database

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -16,7 +16,8 @@ def categories():
     """
     To render the categories.html
     """
-    return render_template("categories.html")
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories=categories)
 
 
 @app.route("/add_category", methods=["GET", "POST"])
