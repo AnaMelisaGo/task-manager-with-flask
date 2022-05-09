f8651439393086436da780851840d2ca5f68c0a7
AnaMelisaGo <melisago.rockn@gmail.com>
2022-05-09 16:51:50 +0000

    Create add_task.html, add form and functions

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -6,4 +6,15 @@ document.addEventListener('DOMContentLoaded', function() {
     // modal initialization
     let modal = document.querySelectorAll('.modal');
     M.Modal.init(modal);
-  });
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmm, yyyy",
+      i18n: {done: "Select"}
+    });
+
+    // select initialization
+    let select = document.querySelectorAll('select');
+    M.FormSelect.init(select);
+});
