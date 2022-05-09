8097c7e7c333d78bdbcb460f24812ce3f2449ac1
AnaMelisaGo <melisago.rockn@gmail.com>
2022-05-09 16:05:08 +0000

    Edit categories.html

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,9 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
 
-document.addEventListener('DOMContentLoaded', function() {
-  let modal = document.querySelectorAll('.modal');
-  M.Modal.init(modal);
-});
+    // modal initialization
+    let modal = document.querySelectorAll('.modal');
+    M.Modal.init(modal);
+  });
